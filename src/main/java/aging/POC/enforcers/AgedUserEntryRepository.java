60e778c2385df5febe5316ea741f78dbf160a6f8
ADEWT001 <ADEWT001@W7-2UA52217ML.swna.wdpr.disney.com>
2015-09-08 13:32:08 -0700

    got rid of unused methods

--- src/main/java/aging/POC/enforcers/AgedUserEntryRepository.java
+++ src/main/java/aging/POC/enforcers/AgedUserEntryRepository.java
@@ -1,8 +1,6 @@
 package aging.POC.enforcers;
 
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
@@ -22,19 +20,4 @@ public interface AgedUserEntryRepository extends MongoRepository<AgedUserEntry, 
     @Query("{ 'jsonData.user.notificationFlag' :  ?0  }")
     public List<AgedUserEntry> findAllAgingCandidatesByAge(int age);
     
-    /*@Query("{ 'jsonData.user.notificationFlag' : 0 }")
-    public List<AgedUserEntry> findAllNewAgingCandidates();
-    
-    //@Query("{ 'jsonData.user.notificationFlag' : { '$gt' : 60 } }")
-    @Query("{ 'jsonData.user.notificationFlag' :   60  }")
-    public List<AgedUserEntry> findAll60DayAgingCandidates();
-
-    @Query("{ 'jsonData.user.notificationFlag' :  75  }")
-    public List<AgedUserEntry> findAll75DayAgingCandidates();
-    
-    @Query("{ 'jsonData.user.notificationFlag' :  85  }")
-    public List<AgedUserEntry> findAll85DayAgingCandidates();
-    
-    @Query("{ 'jsonData.user.notificationFlag' :  90  }")
-    public List<AgedUserEntry> findAll90DayAgingCandidates();*/
 }
