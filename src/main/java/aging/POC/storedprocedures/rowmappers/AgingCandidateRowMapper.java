64888770ddd4892758bc3b6c080b1a7d3856b9e0
ADEWT001 <ADEWT001@W7-2UA52217ML.swna.wdpr.disney.com>
2015-09-10 11:53:58 -0700

    changed AgedUser and added this rowMapper

---
+++ src/main/java/aging/POC/storedprocedures/rowmappers/AgingCandidateRowMapper.java
@@ -0,0 +1,31 @@
+package aging.POC.storedprocedures.rowmappers;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.util.StringUtils;
+
+public class AgingCandidateRowMapper implements RowMapper {
+
+	private Integer userId;
+	private Integer notificationFlag;
+	private Integer isPub;
+	
+	@Override
+	public Object mapRow(ResultSet rs, int arg1) throws SQLException {
+		AgedUser agedUser =  new AgedUser();
+		
+        this.userId = new Integer(StringUtils.trimAllWhitespace(rs.getString(1))); //location of the userId in the resultSet
+        this.notificationFlag =  new Integer(StringUtils.trimAllWhitespace(rs.getString(2))); //location of the notificationFlag in the resultSet
+        this.isPub = new Integer(StringUtils.trimAllWhitespace(rs.getString(3))); //location of the isPub flag in the resultSet
+        
+        agedUser.setUserId(userId);
+        agedUser.setNotificationFlag(userId);
+        agedUser.setIsPub(isPub);
+        
+        return agedUser;
+	}
+
+
+}
